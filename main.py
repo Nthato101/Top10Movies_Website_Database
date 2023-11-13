770b92aca21d68313e885bf984bbc67b91817be3
Nthato <122855450+Nthato101@users.noreply.github.com>
2023-11-13 08:41:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import requests
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = ''
 Bootstrap5(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movie-collection.db'
